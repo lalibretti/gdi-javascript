1a2a471715d18036b6d6c1de82f357355bbac0fc
Lauren Libretti <lalibretti@yahoo.com>
2023-09-08 17:17:43 -0400

    react updates

---
+++ intro-to-react/class/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Intro to React! 
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to React Docs! 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
